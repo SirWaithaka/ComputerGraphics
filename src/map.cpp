4974b50be2eed1c2a90b3cbfd28cf34e164c8244
craftsmon <craftsmon@outlook.com>
2017-05-09 15:29:29 +0300

    Slightly Working Soln

--- src/map.cpp
+++ src/map.cpp
@@ -1,29 +1,42 @@
-// includes GL/gl.h, GL/glu.h, GL/glx.h
+#include <GL/gl.h>
+#include <GL/glu.h>
 #include <GL/glut.h>
 
-void renderFunction()
-{
+
+void display()
+{  
    glClear(GL_COLOR_BUFFER_BIT);
-   glClearColor(0.0, 0.0, 0.0, 0.0);
-   glColor3f(0.0, 0.0, 0.0);
-   glOrtho(0.0, 1.0, 0.0, 1.0, -1.0, 1.0);
-   glBegin(GL_POLYGON);
-      glVertex3f (0.25, 0.25, 0.0);
-      glVertex3f (0.75, 0.25, 0.0);
-      glVertex3f (0.75, 0.75, 0.0);
-      glVertex3f (0.25, 0.75, 0.0);
+   glBegin(GL_POINTS);
+    glVertex2d(41.899578000000005, 3.9774445);
+    glVertex2d(41.8736506, 3.9379500999999997);
+    glVertex2d(41.8725241, 3.9362340999999996);
+    glVertex2d(41.871452999999995, 3.9339675);
    glEnd();
    glFlush();
+}
+
+void init()
+{
+   // set the background color
+   glClearColor(1.0, 1.0, 1.0, 0.0);
+   // set the drawing color
+   glColor3f(0.0f, 0.0f, 0.0f);
+   // set the dot size
+   glPointSize(4.0);
+   glMatrixMode(GL_PROJECTION);
+   glLoadIdentity();
+   gluOrtho2D(0.0, 800.0, 0.0, 600.0);
+
 }
 
 int main(int argc, char** argv)
 {
    glutInit(&argc, argv);
-   glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
-   glutInitWindowSize(600, 600);
-   glutInitWindowPosition(100,100);
-   glutCreateWindow("OpenGl - Kenya map");
-   glutDisplayFunc(renderFunction);
+   glutInitDisplayMode(GLUT_SINGLE);
+   glutInitWindowPosition(100, 100);
+   glutInitWindowSize(800, 600);
+   glutCreateWindow("Map of Kenya");
+   init();
+   glutDisplayFunc(display);
    glutMainLoop();
-   return 0;
 }
